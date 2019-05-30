1de2f218a6b6581d4749adf434f0ed69c35b0e97
Ben <bwillis@endeavorrobotics.com>
2019-05-30 09:13:02 -0400

    add comp agg exercise

---
+++ cmake-build-debug/CMakeFiles/SoftwareProjects.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SoftwareProjects.dir/main.cpp.o"
+  "SoftwareProjects.pdb"
+  "SoftwareProjects"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SoftwareProjects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/SoftwareProjects.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SoftwareProjects.
+# This may be replaced when dependencies are built.
